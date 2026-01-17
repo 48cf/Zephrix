6fe7d16fe229c0756ea73fac3a199fd997ceb2e6
iretq <me@iretq.dev>
2026-01-17 20:38:50 +0100

    build-support: Add input device flags

--- build-support/qemu.sh
+++ build-support/qemu.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-qemu-system-x86_64 -M q35 -m 8G -smp 8 -enable-kvm -cpu host -serial stdio -hda jinix.img $QEMUFLAGS
+qemu-system-x86_64 \
+    -M q35 \
+    -m 8G \
+    -smp 8 \
+    -enable-kvm \
+    -cpu host \
+    -serial stdio \
+    -drive file=jinix.img,format=raw \
+    -device qemu-xhci,id=xhci \
+    -device usb-kbd,bus=xhci.0,port=1 \
+    -device usb-tablet,bus=xhci.0,port=2 \
+    $QEMUFLAGS
